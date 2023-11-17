3bcb67419fed13387b0f55708ee2533756f99ec2
ryok <m6536061@gmail.com>
2023-11-17 01:34:35 -0800

    Added a unittest for getRandomInt

---
+++ test/get-random-int.test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import { getRandomInt } from '../javascript/utilities';
+
+const testData = [
+  { testCase: "0 <= getRandomInt(5) <= 5", value: 5, expected: [0, 5] },
+  { testCase: "0 <= getRandomInt(100) <= 100", value: 100, expected: [0, 100] },
+  { testCase: "getRandomInt(0) == 0", value: 0, expected: [0, 0] },
+];
+
+testData.forEach((v) => {
+  test(`getRandomInt_${v.testCase}`, () => {
+    const actual = getRandomInt(v.value);
+
+    expect(actual).toBeGreaterThanOrEqual(v.expected[0]);
+    expect(actual).toBeLessThanOrEqual(v.expected[1]);
+  });
+});
