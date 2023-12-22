b400ac594076ae14f92c58887a3fed5892702485
ryok <m6536061@gmail.com>
2023-12-22 00:08:35 -0800

    AWS Secrets Manager added to template.yml

--- after_clone_ec2.sh
+++ after_clone_ec2.sh
@@ -2,6 +2,9 @@
 # You should also set up an .env or similar file
 # Then you can start up servers using docker-compose
 
+# manually run this command because it requires user input
+# sudo apt-get install postgresql-client
+
 sudo chmod 600 .env
 sudo apt install -y docker.io
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
