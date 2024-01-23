f25812f2a43d0372bb3b261eb3213916a0ae98ec
ryok <m6536061@gmail.com>
2024-01-22 20:51:48 -0800

    Added commands to after_clone_ec2.sh (postgres)

--- after_clone_ec2.sh
+++ after_clone_ec2.sh
@@ -10,4 +10,6 @@ sudo apt install -y docker.io
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
+sudo apt install postgresql-client-common
+sudo apt-get install postgresql-client-14
 exit
