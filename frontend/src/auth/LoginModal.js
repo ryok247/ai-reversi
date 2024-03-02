a197fdae5144db745d8bfbb932f910c963c910af
ryok <m6536061@gmail.com>
2024-03-02 00:12:29 -0800

    Fix login

--- frontend/src/auth/LoginModal.js
+++ frontend/src/auth/LoginModal.js
@@ -1,32 +1,44 @@
 // LoginModal.js
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import { useDispatch } from 'react-redux';
+import { loginSuccess } from '../actions/authActions';
 import { getCookie } from '../utilities';
 
 const LoginModal = ({ isOpen, onRequestClose }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+  
+    // FormData オブジェクトを使用してフォームデータを作成
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+  
+    // リクエストを送信
     const response = await fetch('/login/', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
         'X-CSRFToken': getCookie('csrftoken'),
       },
-      body: JSON.stringify({ username, password }),
+      body: formData, // JSONではなくformDataを使用
       credentials: 'include',
     });
-    const data = await response.json();
-    if (data.status === 'success') {
+    
+    if (response.ok) { // response.status === 'success'ではなく、response.okを使用
+      const data = await response.json(); // レスポンスデータを取得
+      dispatch(loginSuccess(data.user)); // loginSuccessアクションをディスパッチしてユーザー情報を更新
       onRequestClose(); // モーダルを閉じる
       window.location.href = '/'; // ログイン成功後のリダイレクト
     } else {
       setError('ログインに失敗しました');
     }
   };
+  
 
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} contentLabel="Login">
